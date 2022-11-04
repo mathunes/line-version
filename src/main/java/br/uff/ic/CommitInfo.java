8c58b07e884f6b9793351f5b58d7e994de220e40
mathunes <matheusantunes720@gmail.com>
2022-11-04 12:42:55 -0300

    add CommitInfo class

---
+++ src/main/java/br/uff/ic/CommitInfo.java
@@ -0,0 +1,40 @@
+public class CommitInfo {
+    
+    private String author;
+    private String date;
+    private String commitMessage;
+    private String content;
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCommitMessage() {
+        return this.commitMessage;
+    }
+
+    public void setCommitMessage(String commitMessage) {
+        this.commitMessage = commitMessage;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+}
