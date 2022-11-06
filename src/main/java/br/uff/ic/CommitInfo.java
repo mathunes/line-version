578505ff73ccbaa95057640a6520949d3c683873
mathunes <matheusantunes720@gmail.com>
2022-11-06 00:08:38 -0300

    add getLineObject method to build line to array

--- src/main/java/br/uff/ic/CommitInfo.java
+++ src/main/java/br/uff/ic/CommitInfo.java
@@ -1,5 +1,7 @@
 package br.uff.ic;
 
+import org.json.JSONObject;
+
 public class CommitInfo {
     
     private String hash;
@@ -39,4 +41,13 @@ public class CommitInfo {
         this.message = message;
     }
 
+    public JSONObject getLineObject(String contentLine) {
+        String content = "\"content\": \""+ contentLine.replaceAll("\"", "\\\\\"").replaceAll("\n", "") +"\",";
+        String author = "\"author\": \""+ this.author.replaceAll("\n", "") +"\",";
+        String date = "\"date\": \""+ this.date.replaceAll("\n", "") +"\",";
+        String message = "\"message\": \""+ this.message.replaceAll("\n", "") +"\",";
+        String hash = "\"hash\": \""+ this.hash.replaceAll("\n", "") +"\"";
+
+        return new JSONObject("{" + content + author + date + message + hash + "}");
+    }
 }
